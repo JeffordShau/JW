3f376fb83100b37421a8ebbf686f702bf17951bf
JeffordShau <jshau30@stuy.edu>
2022-01-13 16:38:16 -0500

    egE

--- Woo.java
+++ Woo.java
@@ -4,3 +4,51 @@ APCS
 FP -- Show Us What You Got
 2022-01-13
 */
+
+import java.io*;
+import java utils;
+
+public class Woo
+{
+
+// Instance variables
+  private Protagonist mc;
+  private Monster monster;
+
+  private boolean shop;
+  private boolean gameOver;
+
+  private InputStreamReader isr;
+  private BufferedReader in;
+
+  public Woo()
+  {
+    gameOver = false;
+    isr = new InputStreamReader( System.in );
+    in = new BufferedReader( isr );
+  }
+
+  public void newGame()
+  {
+    String s;
+    String name = "";
+    s = "The Hero has arrived!";
+    s += "\nChoose your hero: \n";
+    s += "\t"
+// implement hero classes
+    System.out.print(s);
+
+    try 
+
+
+
+
+  }
+
+
+
+
+
+
+
+}
